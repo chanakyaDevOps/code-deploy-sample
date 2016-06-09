a495a3320b80dd2af1dac9f3276a3cda17a6ef46
root <root@ip-172-31-54-33.ec2.internal>
2016-06-09 14:30:08 +0000

    Restructured

---
+++ scripts/stop_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if service --status-all | grep -Fq "apache2" ; then
+	service apache2 stop
+fi
--- stop_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if service --status-all | grep -Fq 'apache2'; then    
-  service apache2 stop
-fi
-
